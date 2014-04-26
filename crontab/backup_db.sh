65046a094bf14e9fb7fbce0dc95ea0209aff3209
bigo <bingdong13@msn.com>
2014-04-26 18:14:33 +0800

    Delete backup_db.sh

--- crontab/backup_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-backupDbDir=/app/backup/db
-
-mysqldump -uroot -pcsmysql weitingba > $backupDbDir/weitingba`date -d "-1 days" +%Y%m%d`.sql
-
-find $backupDbDir/ -type f -mtime +30 -exec rm {} \; >/dev/null 2>&1
